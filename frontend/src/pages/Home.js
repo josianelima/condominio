6552f979e149348d48bf3308d071136d23d14b40
Ali <alikhankv@hotmail.com>
2022-02-03 16:57:07 +0000

    novo1

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Topbar from '../components/Topbar';
 import Main from '../components/Main'
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
 
 export default function Home() {
   return (
